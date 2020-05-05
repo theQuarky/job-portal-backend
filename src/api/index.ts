88fe3704db0d48553eedc4beabb9bdcd8438d331
ranahiren27 <ranahiren27@gmail.com>
2020-05-06 01:12:47 +0530

    hosting

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,6 +3,12 @@ import employer from './employerRoute';
 import candidate from './candidateRoute';
 const router: Router = Router();
 
+router.get('/', (req,res)=>{
+    res.send({
+        msg : 'working!'
+    });
+})
+
 router.use('/employer', employer);
 router.use('/candidate', candidate);
 
